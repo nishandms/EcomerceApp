032591feb0a4303cc13f0b7547f5772e023125e0
nishandms <nishandms06@gmail.com>
2022-07-28 00:15:26 +0530

    added product add page and wrote its rest

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express_hbs = require('express-handlebars')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fileupload = require("express-fileupload")
 
 var adminRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
@@ -13,32 +14,33 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs', express_hbs.engine({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname+ '/views/layout/', partialsDir:__dirname+"/views/partials/"}))
+app.engine('hbs', express_hbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout/', partialsDir: __dirname + "/views/partials/" }))
 app.enabled()
 
 app.use(logger('dev'));
+app.use(fileupload())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/',  usersRouter);
+app.use('/', usersRouter);
 app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin', { title: 'Ekart', user: false });
+    res.render('admin', { title: 'Ekart', user: false });
 });
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product')
+})
+router.post('/save-product', (req, res, next) => {
+    console.log("callsed")
+    console.log(req.body)
+        // res.render('add-product')
+})
 
 module.exports = router;
